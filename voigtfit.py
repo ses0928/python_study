494041768deeb08d4f11e7bd2a6fa86c04f39845
your-sql <fluidam23@hanmail.net>
2022-05-02 15:22:02 +0900

    update : added voigt funciton (not psdvoigt)

--- voigtfit.py
+++ voigtfit.py
@@ -3,6 +3,7 @@
 import math
 import numpy as np
 from scipy.optimize import curve_fit
+from scipy.special import erfc
 import sys
 
 # for GUI
@@ -24,20 +25,53 @@ How to use this program
 '''
 
 
-# voigt function definition
-def voigt(x,mu,w,A,xc,backg):
+# pseudo voigt function definition
+def psdvoigt(x,mu,w,A,xc,backg):
     return A*(mu*2/math.pi*w/(4*(x-xc)**2+w**2) + (1-mu)*math.sqrt(4*math.log(2)/math.pi)/w*np.exp(-4*math.log(2)/w**2*(x-xc)**2)) + backg
 
-def voigt2p(x,mu1,w1,A1,xc1,mu2,w2,A2,xc2,backg):
+def psdvoigt2p(x,mu1,w1,A1,xc1,mu2,w2,A2,xc2,backg):
     v1 = A1*(mu1*2/math.pi*w1/(4*(x-xc1)**2+w1**2) + (1-mu1)*math.sqrt(4*math.log(2)/math.pi)/w1*np.exp(-4*math.log(2)/w1**2*(x-xc1)**2))
     v2 = A2*(mu2*2/math.pi*w2/(4*(x-xc2)**2+w2**2) + (1-mu2)*math.sqrt(4*math.log(2)/math.pi)/w2*np.exp(-4*math.log(2)/w2**2*(x-xc2)**2))
     return v1+v2+backg
 
-def voigt3p(x,mu1,w1,A1,xc1,mu2,w2,A2,xc2,mu3,w3,A3,xc3,backg):
+def psdvoigt3p(x,mu1,w1,A1,xc1,mu2,w2,A2,xc2,mu3,w3,A3,xc3,backg):
     v1 = A1*(mu1*2/math.pi*w1/(4*(x-xc1)**2+w1**2) + (1-mu1)*math.sqrt(4*math.log(2)/math.pi)/w1*np.exp(-4*math.log(2)/w1**2*(x-xc1)**2))
     v2 = A2*(mu2*2/math.pi*w2/(4*(x-xc2)**2+w2**2) + (1-mu2)*math.sqrt(4*math.log(2)/math.pi)/w2*np.exp(-4*math.log(2)/w2**2*(x-xc2)**2))
     v3 = A3*(mu3*2/math.pi*w3/(4*(x-xc3)**2+w3**2) + (1-mu3)*math.sqrt(4*math.log(2)/math.pi)/w3*np.exp(-4*math.log(2)/w3**2*(x-xc3)**2))
     return v1+v2+v3+backg
+
+# voigt function definition
+def voigt(xx,s,g,A,xc,backg):
+    x = np.array(xx)-xc
+    z = (x+complex(0,1)*g)/(s*np.sqrt(2))
+    w_z = np.exp(-x*x/(s*np.sqrt(2)))*erfc(-z*complex(0,1))
+    return A*w_z.real/(s*np.sqrt(2*np.pi)) + backg
+
+def voigt2p(xx,s1,g1,A1,xc1,s2,g2,A2,xc2,backg):
+    x1 = np.array(xx)-xc1
+    z1 = (x1+complex(0,1)*g1)/(s1*np.sqrt(2))
+    w_z1 = np.exp(-x1*x1/(s1*np.sqrt(2)))*erfc(-z1*complex(0,1))
+
+    x2 = np.array(xx)-xc2
+    z2 = (x2+complex(0,1)*g2)/(s2*np.sqrt(2))
+    w_z2 = np.exp(-x2*x2/(s2*np.sqrt(2)))*erfc(-z2*complex(0,1))
+
+    return A1*w_z1.real/(s1*np.sqrt(2*np.pi)) + A2*w_z2.real/(s2*np.sqrt(2*np.pi)) + backg
+
+def voigt3p(xx,s1,g1,A1,xc1,s2,g2,A2,xc2,s3,g3,A3,xc3,backg):
+    x1 = np.array(xx)-xc1
+    z1 = (x1+complex(0,1)*g1)/(s1*np.sqrt(2))
+    w_z1 = np.exp(-x1*x1/(s1*np.sqrt(2)))*erfc(-z1*complex(0,1))
+
+    x2 = np.array(xx)-xc2
+    z2 = (x2+complex(0,1)*g2)/(s2*np.sqrt(2))
+    w_z2 = np.exp(-x2*x2/(s2*np.sqrt(2)))*erfc(-z2*complex(0,1))
+    
+    x3 = np.array(xx)-xc3
+    z3 = (x3+complex(0,1)*g3)/(s3*np.sqrt(2))
+    w_z3 = np.exp(-x3*x3/(s3*np.sqrt(2)))*erfc(-z3*complex(0,1))
+    return A1*w_z1.real/(s1*np.sqrt(2*np.pi)) + A2*w_z2.real/(s2*np.sqrt(2*np.pi)) + A3*w_z3.real/(s3*np.sqrt(2*np.pi)) + backg
+
 
 # x, y data containers
 xdata = []
@@ -50,11 +84,15 @@ ydata_temp = []
 para_set = []
 para_sets = []
 
-# loaded files list (tuple type)
+# loaded files list (tuple type), fitting method list
 loaded_files = ("hello","world")
+method = ("voigt","psdvoigt")
 
 # the number of loaded files
 num_files = 0
+
+# function setting : voigt or pseudo voigt
+func = "voigt"
 
 # open file dialog
 def openfiles():
@@ -182,6 +220,8 @@ def draw_fit():
     global ydata_temp
     global para_set
 
+    global func
+
     if len(xdata_temp):
         # 1 : plot raw data
         ax1.clear()
@@ -209,32 +249,56 @@ def draw_fit():
                 fit_subset3.append(para_set[-1])
 
         # data setting
-        if peak_num == 1:
-            [mu,w,A,xc] = para_set[0:4]
+        if func == "psdvoigt":
+            if peak_num == 1:
+                [mu,w,A,xc] = para_set[0:4]
             
-            for i in range(len(xdata_temp)):
-                x = xdata_temp[i]
-                fit_result[i] += A*(mu*2/math.pi*w/(4*(x-xc)**2+w**2) + (1-mu)*math.sqrt(4*math.log(2)/math.pi)/w*np.exp(-4*math.log(2)/w**2*(x-xc)**2))
+                for i in range(len(xdata_temp)):
+                    x = xdata_temp[i]
+                    fit_result[i] += A*(mu*2/math.pi*w/(4*(x-xc)**2+w**2) + (1-mu)*math.sqrt(4*math.log(2)/math.pi)/w*np.exp(-4*math.log(2)/w**2*(x-xc)**2))
             
-        elif peak_num == 2:
-            [mu1,w1,A1,xc1,mu2,w2,A2,xc2] = para_set[0:8]
+            elif peak_num == 2:
+                [mu1,w1,A1,xc1,mu2,w2,A2,xc2] = para_set[0:8]
             
-            for i in range(len(xdata_temp)):
-                x = xdata_temp[i]
-                fit_subset1[i] += A1*(mu1*2/math.pi*w1/(4*(x-xc1)**2+w1**2) + (1-mu1)*math.sqrt(4*math.log(2)/math.pi)/w1*np.exp(-4*math.log(2)/w1**2*(x-xc1)**2))
-                fit_subset2[i] += A2*(mu2*2/math.pi*w2/(4*(x-xc2)**2+w2**2) + (1-mu2)*math.sqrt(4*math.log(2)/math.pi)/w2*np.exp(-4*math.log(2)/w2**2*(x-xc2)**2))
-                fit_result[i] = fit_subset1[i] + fit_subset2[i] - para_set[-1]
+                for i in range(len(xdata_temp)):
+                    x = xdata_temp[i]
+                    fit_subset1[i] += A1*(mu1*2/math.pi*w1/(4*(x-xc1)**2+w1**2) + (1-mu1)*math.sqrt(4*math.log(2)/math.pi)/w1*np.exp(-4*math.log(2)/w1**2*(x-xc1)**2))
+                    fit_subset2[i] += A2*(mu2*2/math.pi*w2/(4*(x-xc2)**2+w2**2) + (1-mu2)*math.sqrt(4*math.log(2)/math.pi)/w2*np.exp(-4*math.log(2)/w2**2*(x-xc2)**2))
+                    fit_result[i] = fit_subset1[i] + fit_subset2[i] - para_set[-1]
         
-        elif peak_num == 3:
-            [mu1,w1,A1,xc1,mu2,w2,A2,xc2,mu3,w3,A3,xc3] = para_set[0:12]
+            elif peak_num == 3:
+                [mu1,w1,A1,xc1,mu2,w2,A2,xc2,mu3,w3,A3,xc3] = para_set[0:12]
 
-            for i in range(len(xdata_temp)):
-                x = xdata_temp[i]
-                fit_subset1[i] += A1*(mu1*2/math.pi*w1/(4*(x-xc1)**2+w1**2) + (1-mu1)*math.sqrt(4*math.log(2)/math.pi)/w1*np.exp(-4*math.log(2)/w1**2*(x-xc1)**2))
-                fit_subset2[i] += A2*(mu2*2/math.pi*w2/(4*(x-xc2)**2+w2**2) + (1-mu2)*math.sqrt(4*math.log(2)/math.pi)/w2*np.exp(-4*math.log(2)/w2**2*(x-xc2)**2))
-                fit_subset3[i] += A3*(mu3*2/math.pi*w3/(4*(x-xc3)**2+w3**2) + (1-mu3)*math.sqrt(4*math.log(2)/math.pi)/w3*np.exp(-4*math.log(2)/w3**2*(x-xc3)**2))
-                fit_result[i] = fit_subset1[i] + fit_subset2[i] + fit_subset3[i] - 2*para_set[-1]
-                
+                for i in range(len(xdata_temp)):
+                    x = xdata_temp[i]
+                    fit_subset1[i] += A1*(mu1*2/math.pi*w1/(4*(x-xc1)**2+w1**2) + (1-mu1)*math.sqrt(4*math.log(2)/math.pi)/w1*np.exp(-4*math.log(2)/w1**2*(x-xc1)**2))
+                    fit_subset2[i] += A2*(mu2*2/math.pi*w2/(4*(x-xc2)**2+w2**2) + (1-mu2)*math.sqrt(4*math.log(2)/math.pi)/w2*np.exp(-4*math.log(2)/w2**2*(x-xc2)**2))
+                    fit_subset3[i] += A3*(mu3*2/math.pi*w3/(4*(x-xc3)**2+w3**2) + (1-mu3)*math.sqrt(4*math.log(2)/math.pi)/w3*np.exp(-4*math.log(2)/w3**2*(x-xc3)**2))
+                    fit_result[i] = fit_subset1[i] + fit_subset2[i] + fit_subset3[i] - 2*para_set[-1]
+            
+        elif func == "voigt":
+            if peak_num == 1:
+                [s,g,A,xc] = para_set[0:4]
+
+                x = np.array(xdata_temp)-xc
+                z = (x+complex(0,1)*g)/(s*np.sqrt(2))
+                w_z = np.exp(-x*x/(s*np.sqrt(2)))*erfc(-z*complex(0,1))
+                fit_result = np.array(A*w_z.real/(s*np.sqrt(2*np.pi))) + para_set[-1]
+            
+            if peak_num == 2:
+                [s1,g1,A1,xc1,s2,g2,A2,xc2] = para_set[0:8]
+
+                x1 = np.array(xdata_temp)-xc1
+                z1 = (x1+complex(0,1)*g1)/(s1*np.sqrt(2))
+                w_z1 = np.exp(-x1*x1/(s1*np.sqrt(2)))*erfc(-z1*complex(0,1))
+                fit_subset1 = np.array(A1*w_z1.real/(s1*np.sqrt(2*np.pi))) + para_set[-1]
+
+                x2 = np.array(xdata_temp)-xc2
+                z2 = (x2+complex(0,1)*g2)/(s2*np.sqrt(2))
+                w_z2 = np.exp(-x2*x2/(s2*np.sqrt(2)))*erfc(-z2*complex(0,1))
+                fit_subset2 = np.array(A2*w_z2.real/(s2*np.sqrt(2*np.pi))) + para_set[-1]
+
+                fit_result = fit_subset1 + fit_subset2 - para_set[-1]
 
         # draw fitting data
         ax1.plot(xdata_temp,fit_result,color='red')
@@ -261,6 +325,8 @@ def voigt_fit(index):
     global ydata_temp
     global para_set
     global filenames
+
+    global func     # determine which function to use
 
     if len(xdata_temp):
         xdata_temp = []
@@ -307,29 +373,60 @@ def voigt_fit(index):
         for i in range(peak_num):
             temp_amplitude = peak_amplitudes[i]/sum_peakA*sum(ydata_temp)/2
             peak_amplitudes[i] = temp_amplitude
-        
+
+        # fitting method setting
+        try:
+            fit = option_fit['menu']
+            value = variable_fit.get()
+            ind = fit.index(value)
+
+            if ind == 0:
+                func = "voigt"
+            elif ind == 1:
+                func = "psdvoigt"
+        except:
+            func = "voigt"
+
         # initial conditions : setting
         initial = []
-        for i in range(peak_num):
-            initial.append(0.5)
-            initial.append(10)
-            initial.append(peak_amplitudes[i])
-            initial.append(peaks[i])
+        if func == "psdvoigt":
+            for i in range(peak_num):
+                initial.append(0.5)
+                initial.append(10)
+                initial.append(peak_amplitudes[i])
+                initial.append(peaks[i])
 
-        initial.append(min(ydata_temp)/2)
+            initial.append(min(ydata_temp)/2)
+        
+        elif func == "voigt":
+            for i in range(peak_num):
+                initial.append(50)
+                initial.append(50)
+                initial.append(peak_amplitudes[i])
+                initial.append(peaks[i])
+            
+            initial.append(min(ydata_temp)/2)
 
         # lower, upper bound setting for voigt fitting
         lb = []
         ub = []
 
         for i in range(peak_num):
-            lb.append(0)
-            lb.append(1)
+            if func == "psdvoigt":
+                lb.append(0)
+                ub.append(1)
+            elif func == "voigt":
+                lb.append(0.1)
+                ub.append(300)
+
+            lb.append(0.1)
             lb.append(sum(ydata_temp)/1000)
             lb.append(peaks[i]-10)
-            ub.append(1)
-            ub.append(50)
-            ub.append(sum(ydata_temp))
+
+            # upper bound for peak width.
+            if func == "psdvoigt": ub.append(50)
+            elif func == "voigt": ub.append(300)
+            ub.append(sum(ydata_temp)*10)
             ub.append(peaks[i]+10)
 
         lb.append(0)
@@ -343,30 +440,58 @@ def voigt_fit(index):
         file_name = name[-1]
 
         # fitting part
-        if peak_num == 1:
-            popt, pcov = curve_fit(voigt,xdata_temp,ydata_temp,p0=initial,bounds=(lb_tuple,ub_tuple))
-            print(file_name)
-            print(f"1st peak width : {popt[1]:.2f}, 1st peak area : {popt[2]:.2f}, 1st peak position : {popt[3]:.2f} cm-1")
-            print(f"background : {popt[-1]:.3f}\n")
+        if func == "psdvoigt":
+            if peak_num == 1:
+                popt, pcov = curve_fit(psdvoigt,xdata_temp,ydata_temp,p0=initial,bounds=(lb_tuple,ub_tuple))
+                print(file_name)
+                print(f"1st peak width : {popt[1]:.2f}, 1st peak area : {popt[2]:.2f}, 1st peak position : {popt[3]:.2f} cm-1")
+                print(f"background : {popt[-1]:.3f}\n")
         
-        elif peak_num == 2:
-            popt, pcov = curve_fit(voigt2p,xdata_temp,ydata_temp,p0=initial,bounds=(lb_tuple,ub_tuple))
-            print(file_name)
-            print(f"1st peak width : {popt[1]:.2f}, 1st peak area : {popt[2]:.2f}, 1st peak position : {popt[3]:.2f} cm-1")
-            print(f"2nd peak width : {popt[5]:.2f}, 2nd peak area : {popt[6]:.2f}, 2nd peak position : {popt[7]:.2f} cm-1")
-            print(f"background : {popt[-1]:.3f}\n")
+            elif peak_num == 2:
+                popt, pcov = curve_fit(psdvoigt2p,xdata_temp,ydata_temp,p0=initial,bounds=(lb_tuple,ub_tuple))
+                print(file_name)
+                print(f"1st peak width : {popt[1]:.2f}, 1st peak area : {popt[2]:.2f}, 1st peak position : {popt[3]:.2f} cm-1")
+                print(f"2nd peak width : {popt[5]:.2f}, 2nd peak area : {popt[6]:.2f}, 2nd peak position : {popt[7]:.2f} cm-1")
+                print(f"background : {popt[-1]:.3f}\n")
             
-            # print(popt)
+                # print(popt)
         
-        elif peak_num == 3:
-            popt, pcov = curve_fit(voigt3p,xdata_temp,ydata_temp,p0=initial,bounds=(lb_tuple,ub_tuple))
-            print(file_name)
-            print(f"1st peak width : {popt[1]:.2f}, 1st peak area : {popt[2]:.2f}, 1st peak position : {popt[3]:.2f} cm-1")
-            print(f"2nd peak width : {popt[5]:.2f}, 2nd peak area : {popt[6]:.2f}, 2nd peak position : {popt[7]:.2f} cm-1")
-            print(f"2nd peak width : {popt[9]:.2f}, 2nd peak area : {popt[10]:.2f}, 2nd peak position : {popt[11]:.2f} cm-1")
-            print(f"background : {popt[-1]:.3f}\n")
+            elif peak_num == 3:
+                popt, pcov = curve_fit(psdvoigt3p,xdata_temp,ydata_temp,p0=initial,bounds=(lb_tuple,ub_tuple))
+                print(file_name)
+                print(f"1st peak width : {popt[1]:.2f}, 1st peak area : {popt[2]:.2f}, 1st peak position : {popt[3]:.2f} cm-1")
+                print(f"2nd peak width : {popt[5]:.2f}, 2nd peak area : {popt[6]:.2f}, 2nd peak position : {popt[7]:.2f} cm-1")
+                print(f"3rd peak width : {popt[9]:.2f}, 3rd peak area : {popt[10]:.2f}, 3rd peak position : {popt[11]:.2f} cm-1")
+                print(f"background : {popt[-1]:.3f}\n")
 
-        para_set = popt
+            para_set = popt
+        
+        elif func == "voigt":
+            if peak_num == 1:
+                popt, pcov = curve_fit(voigt,xdata_temp,ydata_temp,p0=initial,bounds=(lb_tuple,ub_tuple))
+                popt_p = np.array(popt)/(2*np.pi)
+                print(file_name)
+                print(f"1st peak sigma : {popt_p[0]:.2f}, 1st peak gamma : {popt_p[1]:.2f}, 1st peak area : {popt_p[2]:.2f}, 1st peak position : {popt_p[3]:.2f} cm-1")
+                print(f"background : {popt[-1]:.3f}\n")
+            
+            elif peak_num == 2:
+                popt, pcov = curve_fit(voigt2p,xdata_temp,ydata_temp,p0=initial,bounds=(lb_tuple,ub_tuple))
+                popt_p = np.array(popt)/(2*np.pi)
+                print(file_name)
+                print(f"1st peak sigma : {popt_p[0]:.2f}, 1st peak gamma : {popt_p[1]:.2f}, 1st peak area : {popt_p[2]:.2f}, 1st peak position : {popt_p[3]:.2f} cm-1")
+                print(f"2nd peak sigma : {popt_p[4]:.2f}, 2nd peak gamma : {popt_p[5]:.2f}, 2nd peak area : {popt_p[6]:.2f}, 2nd peak position : {popt_p[7]:.2f} cm-1")
+                print(f"background : {popt[-1]:.3f}\n")
+
+            elif peak_num == 3:
+                popt, pcov = curve_fit(voigt3p,xdata_temp,ydata_temp,p0=initial,bounds=(lb_tuple,ub_tuple))
+                popt_p = np.array(popt)/(2*np.pi)
+                print(file_name)
+                print(f"1st peak sigma : {popt_p[0]:.2f}, 1st peak gamma : {popt_p[1]:.2f}, 1st peak area : {popt_p[2]:.2f}, 1st peak position : {popt_p[3]:.2f} cm-1")
+                print(f"2nd peak sigma : {popt_p[4]:.2f}, 2nd peak gamma : {popt_p[5]:.2f}, 2nd peak area : {popt_p[6]:.2f}, 2nd peak position : {popt_p[7]:.2f} cm-1")
+                print(f"3rd peak sigma : {popt_p[8]:.2f}, 3rd peak gamma : {popt_p[9]:.2f}, 3rd peak area : {popt_p[10]:.2f}, 3rd peak position : {popt_p[11]:.2f} cm-1")
+                print(f"background : {popt[-1]:.3f}\n")
+            
+            para_set = popt
 
 # call the voigt fit function - single fit, all fit
 def call_voigtfit():
@@ -466,6 +591,12 @@ option_menu = tkinter.OptionMenu(window_main,variable,*loaded_files)
 option_menu.configure(state='normal')
 option_menu.pack()
 
+variable_fit = tkinter.StringVar(window_main)
+variable_fit.set("Fitting method")
+option_fit = tkinter.OptionMenu(window_main,variable_fit,*method)
+option_fit.configure(state='normal')
+option_fit.pack()
+
     # range input
 label_v = ttk.Label(window_main,text="Range (cm\u207B\u00B9) :")
 label_tilde = ttk.Label(window_main,text="~")
@@ -480,6 +611,9 @@ label_peaks = ttk.Label(window_main,text="peak positions (cm\u207B\u00B9) :")
 entry_peaks = tkinter.Entry(window_main,width=20)
 entry_peaks.insert(0,"1752,1720")
 
+    # fitting method input
+label_fit = ttk.Label(window_main,text="Fitting method :")
+
     # input for calculation of coordination number
 label_conc = ttk.Label(window_main,text="concentration (mol %) :")
 label_gamma = ttk.Label(window_main,text="gamma value :")
@@ -491,13 +625,13 @@ entry_gamma.insert(0,"1.4")
 
 # positioning buttons, entry, and labels
 button_open.place(x=20,y=50)
-button_draw.place(x=50,y=150,anchor="nw")
-button_drawfit.place(x=20,y=180,anchor="nw")
-button_drawall.place(x=150,y=180,anchor="nw")
-button_vfit.place(x=150,y=150,anchor="nw")
-button_afit.place(x=220,y=150,anchor="nw")
-button_cn.place(x=30,y=290)
-button_acn.place(x=220,y=290)
+button_draw.place(x=50,y=180,anchor="nw")
+button_drawfit.place(x=20,y=210,anchor="nw")
+button_drawall.place(x=150,y=210,anchor="nw")
+button_vfit.place(x=150,y=180,anchor="nw")
+button_afit.place(x=220,y=180,anchor="nw")
+button_cn.place(x=30,y=320)
+button_acn.place(x=220,y=320)
 
 option_menu.place(x=100,y=50)
 
@@ -509,10 +643,13 @@ canvas1.create_window(240,100,window=entry_ub)
 label_peaks.place(x=10,y=120)
 canvas1.create_window(170,130,window=entry_peaks)
 
-label_conc.place(x=10,y=220)
-label_gamma.place(x=50,y=250)
-canvas1.create_window(130,230,window=entry_conc)
-canvas1.create_window(130,260,window=entry_gamma)
+label_fit.place(x=10,y=150)
+option_fit.place(x=100,y=145)
+
+label_conc.place(x=10,y=250)
+label_gamma.place(x=50,y=280)
+canvas1.create_window(130,260,window=entry_conc)
+canvas1.create_window(130,290,window=entry_gamma)
 
 # toolbar generation -> graph canvas visualization
 toolbar = NavigationToolbar2Tk(canvas,window_main)
